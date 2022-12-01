4dd9c3ee169b41d274fe51d77194ae1a30146ca6
Salvatore Cardamone <sav.cardamone@gmail.com>
2022-12-01 23:12:58 +0000

    Beginnings of the kaleidoscope parser

---
+++ test/parser_test.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file parser_test.cpp
+ * @author Salvatore Cardamone
+ * @brief Unit tests for the Kaleidoscope parser.
+ */
+#include "parser.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(ParserTest, QQ) { ASSERT_EQ(1, 1); }

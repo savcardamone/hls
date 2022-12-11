adcb6efcc5d2eaa44342aeaa398bbd6959145d4d
Salvatore Cardamone <sav.cardamone@gmail.com>
2022-12-11 12:56:17 +0000

    Pretty-fying the CMake configuration. Added convenience script to build/ run tests

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## Convenience script to nuke build/ and rebuild/ test the project.
+
+# Make sure we run everything in the root directory of the project
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Clean up any existing build and remake everything
+rm -rf build
+cmake \
+    -DCMAKE_C_COMPILER="/usr/local/bin/clang" -DCMAKE_CXX_COMPILER="/usr/local/bin/clang++" \
+    -S . -B build
+cmake --build build
+cd build/test && ctest
